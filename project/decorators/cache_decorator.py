996da821bda387bd71a91ec0be6334d0c7a2c079
SvetlanaDryagalova <149708250+SvetlanaDryagalova@users.noreply.github.com>
2025-04-09 16:28:03 +0300

    Update cache_decorator.py

--- project/decorators/cache_decorator.py
+++ project/decorators/cache_decorator.py
@@ -10,35 +10,38 @@ def cache(max_cache_size: int = 0):
         max_cache_size (int) : The maximum size of the cache.
 
     Returns
-        function: The decorated function with caching applied.
+        function : The decorated function with caching applied.
 
-    Raises:
+    Raises
         ValueError
+            If `max_cache_size` is not an integer.
             If `max_cache_size` is a negative integer.
     """
+    if not isinstance(max_cache_size, int):
+        raise ValueError(f"`max_cache_size` must be an integer, got {type(max_cache_size).__name__}")
+    
+    # Проверка на неотрицательность
     if max_cache_size < 0:
         raise ValueError("The parameter must be a non-negative integer")
 
-    def _cache(func):
-        cached = OrderedDict()
+    cached = {}
 
+    def decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
+            # Формируем ключ для кэша
             kwargs_key = frozenset(kwargs.items())
             key = (args, kwargs_key)
-
+            
+            # Проверяем наличие ключа в кэше
             if key in cached:
-                return cache[key]
-
+                return cached[key]
+            
+            # Вычисляем результат и сохраняем его в кэш
             result = func(*args, **kwargs)
-
             cached[key] = result
-
-            if max_cache_size > 0 and len(cached) > max_cache_size:
-                cached.popitem(last=False)
-
             return result
-
+        
         return wrapper
-
-    return _cache
+    
+    return decorator
