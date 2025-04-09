08b4aebf0d508abc4c5e1af065c9f86c3474ef19
SvetlanaDryagalova <149708250+SvetlanaDryagalova@users.noreply.github.com>
2025-04-09 15:01:23 +0300

    Update vector_operations.py

--- project/vector_operations.py
+++ project/vector_operations.py
@@ -47,10 +47,18 @@ def vector_length(v: List[float]):
 
      v: List[float]  (the input vector)
 
+    Raises
+
+     ValueError
+
+        If the length of one of the vectors is 0.
+
     Returns
 
      float (the length of the vector)
     """
+    if not v:
+        raise ValueError("Vector must not be empty!")
     return sqrt(sum(v[i] ** 2 for i in range(len(v))))
 
 
@@ -80,4 +88,12 @@ def angle_vectors(v: List[float], u: List[float]):
 
         If the lengths of the vectors are not equal.
     """
+    if any(not isinstance(el, float) for el in v) or any(
+        not isinstance(el, float) for el in u
+    ):
+        raise TypeError("Coordinates of the vector must be float!")
+    if v is None or u is None:
+        raise ValueError("Vector must not be empty!")
+    if len(v) != len(u):
+        raise ValueError("Vectors must have the same length!")
     return degrees(acos(dot_product(v, u) / (vector_length(v) * vector_length(u))))
